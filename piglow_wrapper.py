53d18c0e02293c4f19dee55fb0716f1dfc7aa851
Andrew Tatham <andrewtatham@gmail.com>
2016-10-16 21:04:28 +0100

    piglow

--- piglow_wrapper.py
+++ piglow_wrapper.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 import logging
 
 
@@ -6,8 +7,16 @@ class PiGlowWrapper(object):
     def __init__(self, pg):
         self._piglow = pg
         self.v = 8
+        self._init()
         self.every_hour()
         self.every_minute()
+
+    def _init(self):
+        for _ in range(2):
+            self._piglow.all(1)
+            time.sleep(1)
+            self._piglow.all(0)
+            time.sleep(1)
 
     def every_hour(self):
         hour = datetime.datetime.now().hour % 12
@@ -28,8 +37,8 @@ def get():
     try:
         import sys
         sys.path.append("piglow")
-        from piglow import PiGlow
-        return PiGlowWrapper(PiGlow())
+        import piglow
+        return PiGlowWrapper(piglow.PiGlow())
     except Exception as ex:
         logging.warning(ex)
 
