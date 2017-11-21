b353a3bb82e67d322c022729ea332e8ee4ced81b
Andrew Tatham <andrewtatham@gmail.com>
2017-11-21 19:32:52 +0000

    xmas tree - sin

--- xmas_tree.py
+++ xmas_tree.py
@@ -2,9 +2,21 @@ from gpiozero import LEDBoard
 from gpiozero.tools import random_values
 from signal import pause
 
-if __name__ == '__main__':
-    tree = LEDBoard(*range(2, 28), pwm=True)
+
+def random():
     for led in tree:
         led.source_delay = 0.1
         led.source = random_values()
+
+
+def sin():
+    for led in tree:
+        led.source_delay = 0.1
+        led.source = sin_values()
+
+
+if __name__ == '__main__':
+    tree = LEDBoard(*range(2, 28), pwm=True)
+    # random()
+    sin()
     pause()
