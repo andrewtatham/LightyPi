1cd353256f7a08e8bae8b3cd09c2f4432cd5af67
Andrew Tatham <andrewtatham@gmail.com>
2019-03-15 05:24:12 +0000

    day factor fix

---
+++ colour_helper_tests.py
@@ -0,0 +1,43 @@
+import datetime
+import unittest
+
+import colour_helper
+
+
+class ColourHelperTestCase(unittest.TestCase):
+
+    def test_get_day_factor_increasing(self):
+        from_dt = datetime.datetime(2019, 3, 15, 06, 00)
+        to_dt = datetime.datetime(2019, 3, 15, 9, 00)
+        range_minutes = (to_dt - from_dt).seconds / 60
+        step_minutes = 45
+
+        date_generated = (from_dt + datetime.timedelta(minutes=mins)
+                          for mins in range(0, range_minutes + 1, step_minutes))
+
+        actual = [colour_helper.get_day_factor(from_dt, now_dt, to_dt, True)
+                  for now_dt in date_generated]
+
+        expected = [0.0, 0.25, 0.5, 0.75, 1.0]
+
+        self.assertSequenceEqual(expected, actual)
+
+    def test_get_day_factor_decreasing(self):
+        from_dt = datetime.datetime(2019, 3, 15, 18, 00)
+        to_dt = datetime.datetime(2019, 3, 15, 21, 00)
+        range_minutes = (to_dt - from_dt).seconds / 60
+        step_minutes = 45
+
+        date_generated = (from_dt + datetime.timedelta(minutes=mins)
+                          for mins in range(0, range_minutes + 1, step_minutes))
+
+        actual = [colour_helper.get_day_factor(from_dt, now_dt, to_dt, False)
+                  for now_dt in date_generated]
+
+        expected = [1.0, 0.75, 0.5, 0.25, 0.0]
+
+        self.assertSequenceEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
