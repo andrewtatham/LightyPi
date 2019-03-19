3194f7bb321931eccce2e3df7aa522c3b29830f9
Andrew Tatham <andrewtatham@gmail.com>
2019-03-19 13:18:56 +0000

    keybow

---
+++ keybow_helper.py
@@ -0,0 +1,11 @@
+import colour_helper
+
+if __name__ == '__main__':
+
+    for i in range(12):
+        h = i / 12
+        s = 1.0
+        v = 255
+        hsv = (h, s, v)
+        rgb = colour_helper.hsv_to_rgb(hsv)
+        print("F{} {}".format(i + 1, rgb))
